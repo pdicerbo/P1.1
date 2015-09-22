cf307065fd4afae5a13158fdff6096f3d20f6daf
pdicerbo <pierluigi.dicerbo@gmail.com>
2015-09-22 23:17:17 +0200

    complete matrix multiplication

--- D6-exercise/matrix_mult.f90
+++ D6-exercise/matrix_mult.f90
@@ -17,33 +17,51 @@ program matrix_mult
 
   C = 0
   
+  call matrix_multiply(A, B, C, ndim)
+  
   print*, A
   print*, B
   print*, C
 
-  call perform_multiply(A, B, C, ndim)
-  
+  ! call print_matrix(C, ndim)
+
   deallocate(A, B)
   
 end program matrix_mult
 
-subroutine perform_multiply(A, B, C, n)
+subroutine matrix_multiply(A, B, C, n)
   implicit none
-  integer(8), intent(in) :: n,
+  integer(8), intent(in) :: n
   real, intent(in) :: A(n, n)
   real, intent(in) :: B(n, n)
   real, intent(inout) :: C(n, n)
   
   integer(8) :: i, j, k
+  real :: tmp_sum = 0
   
   do j = 1, n
      do i = 1, n
-        
+        do k = 1, n
+           tmp_sum = tmp_sum + A(i, k) * B(k, j)
+        end do
+        C(i, j) = tmp_sum
+        tmp_sum = 0
      end do
   end do
 
-end subroutine perform_multiply
+end subroutine matrix_multiply
 
+subroutine print_matrix(A, n)
+  implicit none
+  integer(8), intent(in) :: n
+  real, intent(in) :: A(n, n)
 
-
+  integer(8) :: i, j
   
+  do i = 1, n
+     do j = 1, n
+        print*, A(i, j)
+     end do
+     write(*,*) " "
+  end do
+end subroutine print_matrix
