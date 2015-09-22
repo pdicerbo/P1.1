edb764317a7be3e6c9614a325f22f3670d811b34
pdicerbo <pierluigi.dicerbo@gmail.com>
2015-09-22 17:58:02 +0200

    adding function

--- D6-exercise/matrix_mult.f90
+++ D6-exercise/matrix_mult.f90
@@ -10,7 +10,7 @@ program matrix_mult
   
   do j = 1, ndim
      do i = 1, ndim
-        A(j, i) = (i - 1)*ndim + j - 1
+        A(i, j) = (i - 1)*ndim + j - 1
         B(i, j) = (i - 1)*ndim + j - 1
      end do
   end do
@@ -21,9 +21,28 @@ program matrix_mult
   print*, B
   print*, C
 
+  call perform_multiply(A, B, C, ndim)
+  
   deallocate(A, B)
   
 end program matrix_mult
+
+subroutine perform_multiply(A, B, C, n)
+  implicit none
+  integer(8), intent(in) :: n,
+  real, intent(in) :: A(n, n)
+  real, intent(in) :: B(n, n)
+  real, intent(inout) :: C(n, n)
+  
+  integer(8) :: i, j, k
+  
+  do j = 1, n
+     do i = 1, n
+        
+     end do
+  end do
+
+end subroutine perform_multiply
 
 
 
