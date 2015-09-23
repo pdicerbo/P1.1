07dfc02505c2337d7e29985659aec92f56430bd1
pdicerbo <pierluigi.dicerbo@gmail.com>
2015-09-23 16:54:44 +0200

    transpose

--- D8-assignment/first_task.c
+++ D8-assignment/first_task.c
@@ -3,10 +3,12 @@
 
 void initialize_matrix(int*, int, int, int*);
 void print_matrix(int*, int, int);
+int* transpose_matrix(int*, int*, int, int);
 
 int main(){
 
   int *A, *B, *C;
+  int *AT, *BT, *CT;
   int row_a, col_a, row_b, col_b;
   int count = 1;
 
@@ -19,12 +21,19 @@ int main(){
   A = (int* )malloc(row_a * col_a * sizeof(int));
   B = (int* )malloc(row_b * col_b * sizeof(int));
   C = (int* )malloc(row_a * col_b * sizeof(int));
-
+  
   initialize_matrix(A, row_a, col_a, &count);
   print_matrix(A, row_a, col_a);
   initialize_matrix(B, row_b, col_b, &count);
   print_matrix(B, row_b, col_b);
 
+  /* Transpose matrix */
+  AT = (int* )malloc(row_a * col_a * sizeof(int));
+  BT = (int* )malloc(row_b * col_b * sizeof(int));
+  CT = (int* )malloc(row_a * col_b * sizeof(int));  
+
+  AT = transpose_matrix(A, AT, row_a, col_a);
+  print_matrix(AT, col_a, row_a);
   return 0;
 }
 
@@ -49,4 +58,14 @@ void print_matrix(int* matrix, int nr, int nc){
       printf("\t%d", matrix[i*nc + j]);
     printf("\n");
   }
+}
+
+int* transpose_matrix(int* matrix, int* transp, int nr, int nc){
+  int i, j;
+  for(i = 0; i < nr; i++){
+    for(j = 0; j < nc; j++){
+      transp[j*nr + i] = matrix[i*nc + j];
+    }
+  }
+  return transp;
 }
