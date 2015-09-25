ff8e34ce986e986606317e44d88cf7121005090f
pdicerbo <pierluigi.dicerbo@gmail.com>
2015-09-25 16:28:40 +0200

    some modifications

--- D8-assignment/CMake_Folder/second_task.F90
+++ D8-assignment/CMake_Folder/second_task.F90
@@ -5,7 +5,8 @@ program second_task
   integer(c_int) :: row_a, col_a, row_b, col_b
   integer :: i, j
   real(8) :: count
-  
+
+#ifndef CBLASFOUND  
   interface
      subroutine mm_multiply(A, B, C, m, n, k) BIND(C)
        use iso_c_binding
@@ -13,6 +14,7 @@ program second_task
        integer(c_int), value :: m, n, k
      end subroutine mm_multiply
   end interface
+#endif
   
   row_a = 2
   col_a = 3
@@ -36,7 +38,6 @@ program second_task
      end do
   end do
 
-  call mm_multiply(c_loc(A), c_loc(B), c_loc(C), row_a, col_a, col_b);
   write(*,*)
   write(*,*) "Printing first matrix:"
   print*, A
@@ -44,16 +45,17 @@ program second_task
   write(*,*) "Printing second matrix:"
   print*, B
   write(*,*)
-  write(*,*) "Printing product matrix:"
+
+#ifndef CBLASFOUND
+  call mm_multiply(c_loc(A), c_loc(B), c_loc(C), row_a, col_a, col_b);
+  write(*,*) "Printing product matrix using C routine:"
   print*, C
-
-  write(*,*)
-  write(*,*) "Performing matrix matrix multiplication with blas routine"
-  write(*,*) "Printing result:"  
-
+#else  
   call dgemm('n', 'n', row_a, col_b, col_a, 1.0d+0, A, row_a, B, row_b, 0.0d+0, C, row_a)
+  write(*,*) "Printing product matrix using BLAS routine:"
   print*, C
-
+#endif
+  
   deallocate(A, B, C)
   
 end program
